c4563f822c4048acb502e49b42f662ea57125c9f
lr222gw <lowe.raivio@gmail.com>
2023-12-22 21:13:26 +0100

    FIX: Remove layout type from  helper functions

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -2,12 +2,12 @@ from . import data_class as d
 from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QVBoxLayout
 
 
-def addButton(text:str, layout:QVBoxLayout) -> QPushButton:
+def addButton(text:str, layout) -> QPushButton:
     newButton = QPushButton(text)
     layout.addWidget(newButton)
     return newButton
 
-def addTextField(fieldName:str, parentLayout:QVBoxLayout) -> d.GuiData:
+def addTextField(fieldName:str, parentLayout) -> d.GuiData:
     
     # Create Label, input field widgets
     newFieldLabel=QLabel(text=fieldName)
