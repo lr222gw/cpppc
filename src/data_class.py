ba1e3b91ebb1171767b5a595b6e4328339c877c9
lr222gw <lowe.raivio@gmail.com>
2023-12-22 20:00:55 +0100

    ADD: introduced a GUIData struct

---
+++ src/data_class.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QLabel, QWidget
+from dataclasses import dataclass
+
+# NOTE: Class may be skipped if it remains this empty
+@dataclass
+class GuiData:
+    widget: QWidget
