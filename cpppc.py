09f169e004876fb765dc10cdb3292c6ba2c06825
lr222gw <lowe.raivio@gmail.com>
2023-12-22 21:13:56 +0100

    WIP: More exampels, layout changes

--- cpppc.py
+++ cpppc.py
@@ -1,20 +1,37 @@
 from src import helper_funcs as hlp, action_funcs as act, data_class as d
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout,QVBoxLayout, QHBoxLayout
 
 app = QApplication([])
 app.setStyleSheet("QPushButton { color: magenta; }")
 
 # Preparing Window
 window = QWidget()
-layout = QVBoxLayout()
+rootLayout = QHBoxLayout()
+projectNameLayout = QFormLayout()
+cmakeVersionLayout = QFormLayout()
 
 # Declare Project Name
-project_name = hlp.addTextField("Project Name:", layout)
+project_name = hlp.addTextField("Project Name:", projectNameLayout)
+
+# Declare Target dir
+target_dir = hlp.addTextField("Target directory:", projectNameLayout)
+
+executable_name = hlp.addTextField("Executable Name:", projectNameLayout)
+
+cmake_description = hlp.addTextField("Project Description:", projectNameLayout)
+
+cmake_version_major = hlp.addTextField("CMake Major Version:", cmakeVersionLayout)
+cmake_version_minor = hlp.addTextField("CMake Minor Version:", cmakeVersionLayout)
+cmake_version_patch = hlp.addTextField("CMake Patch Version:", cmakeVersionLayout)
+
 
 # Create button, run test function on press
-butt = hlp.addButton("Run", layout)
+butt = hlp.addButton("Run", projectNameLayout)
 butt.clicked.connect(lambda: act.test(project_name) )
 
-window.setLayout(layout)
+rootLayout.addLayout(projectNameLayout)
+rootLayout.addLayout(cmakeVersionLayout)
+
+window.setLayout(rootLayout)
 window.show()
 app.exec()
