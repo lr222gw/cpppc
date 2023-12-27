5c06126eb272bd3f8b363ee2e885dd79e6a34e82
lr222gw <lowe.raivio@gmail.com>
2023-12-27 19:51:05 +0100

    ADD: ProjectConfigurationData class, to store user configs

---
+++ src/structs/ProjectConfigurationData.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass, field
+from .CMakeVersionData import CMakeVersionData
+from .GuiData import GuiData
+
+
+
+@dataclass
+class ProjectConfigurationData: 
+    projectName :       GuiData= field(default= GuiData)
+    def projectName_str(self) -> str:
+        return self.projectName.widget.text()
+
+    projectTargetDir :  GuiData= field(default= GuiData)
+    def projectTargetDir_str(self) -> str:
+        return self.projectTargetDir.widget.text()
+
+    projectExecName :   GuiData= field(default= GuiData)
+    def projectExecName_str(self) -> str:
+        return self.projectExecName.widget.text()
+
+    projectDesc :       GuiData= field(default= GuiData)
+    def projectDesc_str(self) -> str:
+        return self.projectDesc.widget.toPlainText()
+
+    overwriteProjectTargetDir : GuiData = field(default= GuiData)
+    def toggle_overwriteProjectTargetDir(self):
+        self.overwriteProjectTargetDir.widget.setCheckState(not self.overwriteProjectTargetDir.widget.isChecked())
+    def get_overwriteProjectTargetDir(self):
+        return self.overwriteProjectTargetDir.widget.isChecked()
+
+    #TODO: put inside a vector, enable user to use multiple prgorams...
+    useProgram_ccache : GuiData = field(default= GuiData)
+    def toggle_useProgram_ccache(self):
+        self.useProgram_ccache.widget.setCheckState(not self.useProgram_ccache.widget.isChecked())
+    def get_useProgram_ccache(self):
+        return self.useProgram_ccache.widget.isChecked()
+
+        
+
+    def getTargetPath(self) -> str:
+        path = self.projectTargetDir.widget.text() +  "/" + self.projectName.widget.text()
+        return path
+
+    cmakeVersionData :  CMakeVersionData = field(default=CMakeVersionData)
+    def toString(self):
+        print(f"ProjectName:{self.projectName.widget.text()}")
+        print(f"projectTargetDir:{self.projectTargetDir.widget.text()}")
+        print(f"projectExecName:{self.projectExecName.widget.text()}")
+        print(f"projectDesc:{self.projectDesc.widget.toPlainText()}")
+        print(f"cmakeVersionData.major:{self.cmakeVersionData.get_major()}")
+        print(f"cmakeVersionData.minor:{self.cmakeVersionData.get_minor()}")
+        print(f"cmakeVersionData.patch:{self.cmakeVersionData.get_patch()}")
