9ec1dfa052ad453e8df0e8651b3fd17c7d3567f3
lr222gw <lowe.raivio@gmail.com>
2023-12-27 19:54:26 +0100

    FIX/ADD:  Renamed vars, Made use of new helper functions

--- cpppc.py
+++ cpppc.py
@@ -1,5 +1,10 @@
-from src import helper_funcs as hlp, action_funcs as act, data_class as d
-from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout,QVBoxLayout, QHBoxLayout
+from src import helper_funcs as hlp, action_funcs as act
+from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout,QVBoxLayout, QHBoxLayout, QGroupBox, QLineEdit
+
+from src.structs.GuiData import GuiData 
+from src.structs.CMakeVersionData import CMakeVersionData
+from src.structs.ProjectConfigurationData import ProjectConfigurationData
+import src.cmake_helper as  cmake_helper
 
 app = QApplication([])
 app.setStyleSheet("QPushButton { color: magenta; }")
@@ -7,30 +12,48 @@ app.setStyleSheet("QPushButton { color: magenta; }")
 # Preparing Window
 window = QWidget()
 rootLayout = QHBoxLayout()
-projectNameLayout = QFormLayout()
-cmakeVersionLayout = QFormLayout()
+layout_projectName = QFormLayout()
+layout_cmakeVersion = QHBoxLayout()
+
+ProjConfDat = ProjectConfigurationData()
 
 # Declare Project Name
-project_name = hlp.addTextField("Project Name:", projectNameLayout)
+group_projdef = QGroupBox(title="Project config")
+ProjConfDat.projectName = hlp.addTextField("Project Name:", "myProject", layout_projectName)
+
 
 # Declare Target dir
-target_dir = hlp.addTextField("Target directory:", projectNameLayout)
+ProjConfDat.projectTargetDir = hlp.addTextField("Target directory:"      , ".", layout_projectName) #TODO: Check if directory is safe / suitable!
+ProjConfDat.projectExecName  = hlp.addTextField("Executable Name:"       , "executable",layout_projectName)
+ProjConfDat.projectDesc      = hlp.addTextBoxField("Project Description:",layout_projectName)
 
-executable_name = hlp.addTextField("Executable Name:", projectNameLayout)
+group_projdef.setLayout(layout_projectName)
 
-cmake_description = hlp.addTextField("Project Description:", projectNameLayout)
+group_cmake = QGroupBox(title="CMake version")
+cmakeVersion_data = cmake_helper.getCMakeVersion()
+cmake_version_major = hlp.addCmakeVersionBox("Major:", cmakeVersion_data.major, layout_cmakeVersion)
+cmake_version_minor = hlp.addCmakeVersionBox("Minor:", cmakeVersion_data.minor, layout_cmakeVersion)
+cmake_version_patch = hlp.addCmakeVersionBox("Patch:", cmakeVersion_data.patch, layout_cmakeVersion)
+ProjConfDat.cmakeVersionData = cmakeVersion_data #TODO: Remember to update this when user change values...
+group_cmake.setLayout(layout_cmakeVersion)
 
-cmake_version_major = hlp.addTextField("CMake Major Version:", cmakeVersionLayout)
-cmake_version_minor = hlp.addTextField("CMake Minor Version:", cmakeVersionLayout)
-cmake_version_patch = hlp.addTextField("CMake Patch Version:", cmakeVersionLayout)
 
 
 # Create button, run test function on press
-butt = hlp.addButton("Run", projectNameLayout)
-butt.clicked.connect(lambda: act.test(project_name) )
+butt = hlp.addButton("Test", layout_projectName)
+# butt.clicked.connect(lambda: act.test(project_name) )
+butt.clicked.connect(lambda: act.cmakebuttontest() )
 
-rootLayout.addLayout(projectNameLayout)
-rootLayout.addLayout(cmakeVersionLayout)
+
+createProjectButton = hlp.addButton("Create", layout_projectName)
+createProjectButton.clicked.connect(lambda: act.createProject(ProjConfDat))
+
+ProjConfDat.overwriteProjectTargetDir = hlp.addCheckBox("Overwrite", False, layout_projectName)
+ProjConfDat.useProgram_ccache = hlp.addCheckBox("Use CCache", True, layout_projectName)
+# ProjConfDat.overwriteProjectTargetDir.widget.stateChanged.connect(lambda: ProjConfDat.toggle_overwriteProjectTargetDir())
+
+rootLayout.addWidget(group_projdef)
+rootLayout.addWidget(group_cmake)
 
 window.setLayout(rootLayout)
 window.show()
