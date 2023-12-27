02d4ac0cc1b1db9200ba69e50439e05e5f7b1b7d
lr222gw <lowe.raivio@gmail.com>
2023-12-27 19:52:17 +0100

    ADD: More helper functions

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -1,5 +1,5 @@
-from . import data_class as d
-from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QVBoxLayout
+from .structs import GuiData as d
+from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QSpinBox, QFormLayout, QTextEdit, QCheckBox ,QVBoxLayout
 
 
 def addButton(text:str, layout) -> QPushButton:
@@ -7,14 +7,70 @@ def addButton(text:str, layout) -> QPushButton:
     layout.addWidget(newButton)
     return newButton
 
-def addTextField(fieldName:str, parentLayout) -> d.GuiData:
+def addTextField(fieldName:str,placeholder:str, parentLayout) -> d.GuiData:
     
     # Create Label, input field widgets
     newFieldLabel=QLabel(text=fieldName)
     newLineEdit=QLineEdit()
+    newLineEdit.setPlaceholderText(placeholder)
 
     # Append widgets to parent layout
     parentLayout.addWidget(newFieldLabel)
     parentLayout.addWidget(newLineEdit)
 
     return d.GuiData(newLineEdit)
+
+def addTextBoxField(fieldName:str, parentLayout) -> d.GuiData:
+    
+    # Create Label, input field widgets
+    newFieldLabel=QLabel(text=fieldName)
+    newLineEdit=QTextEdit()
+
+    # Append widgets to parent layout
+    parentLayout.addWidget(newFieldLabel)
+    parentLayout.addWidget(newLineEdit)
+
+    return d.GuiData(newLineEdit)
+
+def addSpinBox(fieldName:str, parentLayout) -> d.GuiData:
+    
+    # Create Label, input field widgets
+    newFieldLabel=QLabel(text=fieldName)
+    newSpinBox=QSpinBox()
+
+    # Append widgets to parent layout
+    parentLayout.addWidget(newFieldLabel)
+    parentLayout.addWidget(newSpinBox)
+
+    return d.GuiData(newSpinBox)
+
+def addCheckBox(fieldName:str, defaultValue :bool, parentLayout) -> d.GuiData:
+    
+    # Create Label, input field widgets
+    # newFieldLabel=QLabel(text=fieldName)
+    newCheckBox=QCheckBox(text=fieldName)    
+    newCheckBox.setCheckState(defaultValue)
+    # Append widgets to parent layout
+    # parentLayout.addWidget(newFieldLabel)
+    parentLayout.addWidget(newCheckBox)
+
+    return d.GuiData(newCheckBox)    
+
+def addCmakeVersionBox(fieldName:str, version:QSpinBox, parentLayout) -> d.GuiData:
+    
+    # Create Label, input field widgets
+    newFieldLabel=QLabel(text=fieldName)
+    # newSpinBox=QSpinBox()
+    # newSpinBox.setValue(version)
+
+    newLayout = QFormLayout()
+    
+    newLayout.addWidget(newFieldLabel)
+    # newLayout.addWidget(newSpinBox)
+    newLayout.addWidget(version)
+
+    # Append widgets to parent layout    
+    parentLayout.addLayout(newLayout)
+    
+
+    return d.GuiData(version)    
