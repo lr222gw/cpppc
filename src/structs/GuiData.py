f4e96a5206bceadaedb2082cb4bfa1251178f249
lr222gw <lowe.raivio@gmail.com>
2024-01-03 19:39:09 +0100

    ADD: Feature class

--- src/structs/GuiData.py
+++ src/structs/GuiData.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from PyQt5.QtWidgets import QLabel, QWidget, QComboBox, QCheckBox, QLineEdit
 from dataclasses import dataclass
 from typing import Callable
+from ..dev.Terminate import terminate
 
 # NOTE: Class may be skipped if it remains this empty
 @dataclass
@@ -86,5 +87,30 @@ class PropComboBox(Prop, GuiDataComboBox):
     def setValue(self, index):
         self.widget.setCurrentIndex(index)
 
+    def getValue(self) -> str:
+        return self.widget.currentText()
+
+
+def __defaultFunc__():
+    terminate("Feature function not implemented")
+
+@dataclass
+class Feature(ABC):    
+    func : Callable = __defaultFunc__
+    featureName : str = "<MISSING NAME>"
+    @abstractmethod
+    def setValue(self, value):
+        pass
+    @abstractmethod
+    def getValue(self) -> any:
+        pass
+
+
+
+@dataclass
+class FeatureToggle(Feature, GuiDataToggle):
+    def setValue(self, value):
+        self.widget.setChecked(value)        
+    
     def getValue(self) -> str:
         return self.widget.currentText()
